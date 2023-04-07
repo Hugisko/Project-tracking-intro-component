c33f5bb8f237f8db756f4b0e5e43a8b2d56fb71e
Hung Do Quoc <hung.do.quoc@student.tuke.sk>
2023-04-07 15:56:15 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const nav_btn = document.querySelector('.nav-btn');
+const hamburger = document.querySelector('.hamburger');
+const close_btn = document.querySelector('.close');
+const nav = document.querySelector('nav');
+
+nav_btn.addEventListener('click', () => {
+    hamburger.classList.toggle('hidden');
+    close_btn.classList.toggle('hidden');
+    nav.classList.toggle('hidden');
+});
+
+
+
+window.addEventListener('resize', () => {
+    if(window.innerWidth >= 720) {
+        nav.classList.remove('hidden');
+    } else {
+        if(close_btn.classList.contains('hidden')){
+            nav.classList.add('hidden');
+        }
+    }
+});
